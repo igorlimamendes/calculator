9012b60a1211092079537ded6ab4c516cf85c59b
igorlimamendes <igorlimamendes@protonmail.com>
2021-01-28 13:29:54 -0300

    Add variables

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // Load content before DOM
 document.addEventListener("DOMContentLoaded", () => {
-    
+    // Variables
+    const display = document.querySelector(".display");
+    const buttons = document.querySelectorAll("button");
+    let number1 = "", operator = "", number2 = "", result = "";
 });
